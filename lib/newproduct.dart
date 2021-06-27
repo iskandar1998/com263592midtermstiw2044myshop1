b91106ecc0b4a11e5215bb40565acb85fd2cafea
Iskandar <72860374+iskandar1998@users.noreply.github.com>
2021-06-28 06:33:59 +0800

    second upload

--- lib/newproduct.dart
+++ lib/newproduct.dart
@@ -1,6 +1,51 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-class Newproduct extends StatelessWidget {
+import 'package:com263592midtermstiw2044myshop1/product.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:modal_progress_hud/modal_progress_hud.dart';
+import 'package:provider/provider.dart';
+
+class Newproduct extends StatefulWidget {
+  @override
+  _NewproductState createState() => _NewproductState();
+}
+
+class _NewproductState extends State<Newproduct> {
+  double screenHeight, screenWidth;
+
+  bool _isLoading = false;
+  final _formKey = GlobalKey<FormState>();
+  String _productName, _productType;
+  double _price;
+  int _quantity;
+
+  File _image;
+  final picker = ImagePicker();
+
+  Future getImagefromcamera() async {
+    final pickedImage = await picker.getImage(source: ImageSource.camera);
+    setState(() {
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
+      } else {
+        print("No Image Selected");
+      }
+    });
+  }
+
+  Future getImagefromGallery() async {
+    final pickedImage = await picker.getImage(source: ImageSource.gallery);
+    setState(() {
+      if (pickedImage != null) {
+        _image = File(pickedImage.path);
+      } else {
+        print("No Image Selected");
+      }
+    });
+  }
+
   Widget textField({
     @required String hintText,
   }) {
@@ -40,77 +85,193 @@ class Newproduct extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var prname;
-    var prtype;
-    var prprice;
-    var prqty;
-
+    screenHeight = MediaQuery.of(context).size.height;
+    screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text(
+          'Add New Product',
+        ),
+      ),
       body: SafeArea(
-        child: Container(
-          margin: EdgeInsets.symmetric(horizontal: 50),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Text(
-                "New Product Form",
-                style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35,
+        child: ModalProgressHUD(
+          inAsyncCall: _isLoading,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.end,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Flexible(child: _imageSelector(context)),
+                Flexible(
+                  child: Container(
+                    margin: EdgeInsets.symmetric(horizontal: 18),
+                    child: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              labelText: 'Product Name',
+                              labelStyle: TextStyle(
+                                fontSize: 20,
+                              ),
+                              hintText: 'Product Name',
+                              prefixIcon: Icon(Icons.note),
+                            ),
+                            onSaved: (newValue) => _productName = newValue,
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              labelText: 'Product Type',
+                              labelStyle: TextStyle(
+                                fontSize: 20,
+                              ),
+                              hintText: 'Product Type',
+                              prefixIcon:
+                                  Icon(Icons.local_grocery_store_rounded),
+                            ),
+                            onSaved: (newValue) => _productType = newValue,
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(10),
+                                ),
+                                labelText: 'Price',
+                                labelStyle: TextStyle(
+                                  fontSize: 20,
+                                ),
+                                hintText: 'Price',
+                                prefixIcon: Icon(Icons.attach_money_rounded),
+                                suffix: Text('RM')),
+                            onSaved: (newValue) =>
+                                _price = double.tryParse(newValue),
+                          ),
+                          SizedBox(
+                            height: 18,
+                          ),
+                          TextFormField(
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                              labelText: 'Quantity',
+                              labelStyle: TextStyle(
+                                fontSize: 20,
+                              ),
+                              hintText: 'Quantity',
+                              prefixIcon: Icon(Icons.format_list_numbered),
+                            ),
+                            onSaved: (newValue) =>
+                                _quantity = int.tryParse(newValue),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-              Container(
-                height: 300,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    TextField(
-                      controller: prname,
-                      decoration: InputDecoration(
-                          labelText: 'Product Name',
-                          icon: Icon(Icons.pedal_bike_rounded)),
+                Container(
+                  margin: EdgeInsets.all(18),
+                  width: double.infinity,
+                  child: OutlinedButton(
+                    onPressed: () {
+                      _submit(context);
+                    },
+                    child: Text(
+                      'Submit',
+                      style: TextStyle(fontSize: 24),
                     ),
-                    TextField(
-                      controller: prtype,
-                      decoration: InputDecoration(
-                          labelText: 'Product Type',
-                          icon: Icon(Icons.grade_sharp)),
-                    ),
-                    TextField(
-                      controller: prprice,
-                      decoration: InputDecoration(
-                          labelText: 'Product Price',
-                          icon: Icon(Icons.attach_money_rounded)),
-                    ),
-                    TextField(
-                      controller: prqty,
-                      decoration: InputDecoration(
-                          labelText: 'Product Quanlity',
-                          icon: Icon(Icons.local_grocery_store_rounded)),
-                    ),
-                    SizedBox(height: 15),
-                  ],
+                  ),
                 ),
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  button(
-                      buttonName: "Cancel",
-                      color: Colors.grey,
-                      textColor: Colors.black),
-                  button(
-                      buttonName: "Buy",
-                      color: Colors.red,
-                      textColor: Colors.white),
-                ],
-              )
-            ],
+              ],
+            ),
           ),
         ),
+      ),
+    );
+  }
+
+  void _submit(BuildContext context) async {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      Product product = Product(
+          productName: _productName,
+          productType: _productType,
+          price: _price,
+          quantity: _quantity);
+
+      setState(() {
+        _isLoading = true;
+      });
+      // bool isCreated =
+      //     await Provider.of<ProductProvider>(context, listen: false)
+      //         .addProduct(product, _image);
+      // setState(() {
+      //   _isLoading = false;
+      // });
+      // if (!isCreated)
+      {
+        Fluttertoast.showToast(
+            msg: "Adding product failed",
+            toastLength: Toast.LENGTH_SHORT,
+            gravity: ToastGravity.CENTER,
+            timeInSecForIosWeb: 1,
+            textColor: Colors.black,
+            fontSize: 16.0);
+      }
+      Navigator.of(context).pop();
+    }
+  }
+
+  Container _imageSelector(BuildContext context) {
+    return Container(
+      height: screenHeight / 5,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Container(
+              alignment: Alignment.center,
+              child: Text(
+                "Take picture from:",
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16),
+              )),
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              RaisedButton.icon(
+                icon: Icon(Icons.camera_alt),
+                onPressed: getImagefromcamera,
+                label: Text('Camera'),
+              ),
+              RaisedButton.icon(
+                icon: Icon(Icons.photo_album),
+                onPressed: getImagefromGallery,
+                label: Text('Gallery'),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
